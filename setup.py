d2a771172713d493b75f14db50000a55824370f3
0x1618 <80158535+0x1618@users.noreply.github.com>
2024-03-10 21:52:43 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import find_packages, setup
 setup(
     author='Maksymilian Sawicz',
     name='flask_redis_caching',
-    version='1.0.1',
+    description='Flask Redis Caching is a Python library that provides caching functionalities using Redis for storing and retrieving cached values in Flask applications.',
+    long_description='See https://github.com/0x1618/flask-redis-caching',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=[
         'flask-redis'
