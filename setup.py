2d2c20704981aa702dc4c8ef08b0d3075eb8dab8
0x1618 <80158535+0x1618@users.noreply.github.com>
2024-03-10 19:31:50 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     author='Maksymilian Sawicz',
     name='flask_redis_caching',
-    version='1.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[
         'flask-redis'
